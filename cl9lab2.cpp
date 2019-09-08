9386f73fd04e8fd38afa3e060965c7877b2715b5
Vera Michkovsky <vera.michkovsky@hotmail.com>
2019-09-08 19:53:24 +0300

    implement

--- cl9lab2.cpp
+++ cl9lab2.cpp
@@ -7,6 +7,19 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	double square,perimeter,p, a, b, c;
+
+	cout << "input triangle side a: ";
+	cin >> a;
+	cout << "input triangle side b: ";
+	cin >> b;
+	cout << "input triangle side c: ";
+	cin >> c;
+	perimeter = a + b + c;
+	cout << "Perimeter is: " << perimeter << endl;
+	p = perimeter / 2;
+	square = sqrt(p * (p - a) * (p - b) * (p - c));
+	cout << "Square is: " << square << endl;
+
 	return 0;
 }
